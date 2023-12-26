ab283797b288059e6c3d1fd6f7560e121b740ec3
vqtung2k4 <vuquangtung2004@gmail.com>
2023-12-26 18:33:32 +0700

    MAIN

--- setup.ps1
+++ setup.ps1
@@ -9,6 +9,8 @@ function Write-done { Write-Host "DONE" -ForegroundColor Blue; Write-Host }
 
 
 #Start 
+
+# Admin Access
 Start-Process -Wait powershell -verb runas -ArgumentList "Set-ItemProperty -Path REGISTRY::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System -Name ConsentPromtBehaviorAdmin -Value 0"
 
 Write-Start -msg "Installing Scoop..."
@@ -32,5 +34,25 @@ Write-Start -msg "Installing Scoop's packages"
 	scoop install googlechrome
 	scoop install vscode
 	scoop install vcredist-aio
-	scoop install firefox
+	scoop install winrar
+Write-done
+
+Write-Start -msg "Configuring VSCode"
+	code --install-extension ritwickdey.LiveServer
+Write-done
+
+Write-Start -msg "Enable Virtualization"
+Start-Process -Wait powershell 0verb runas -ArgumentList @"
+	echo y | Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -All"@
+Write-done
+
+Write-Start -msg "Installing WSL..."
+If(!(wsl -l -v)) {
+	wsl --install
+	wsl --update
+	wsl --install --no-launch --web-download -d Ubuntu
+}
+Else {
+	Write-Warning "WSL installed"
+}
 Write-done
